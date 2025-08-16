557b2b6b663d18afd319a688e289fb9b2afa4567
Aayan Khan <aayan.khan02072007@gmail.com>
2025-08-16 20:30:38 +0530

    Initial

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ import secrets
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "null"
+app.config["SESSION_TYPE"] = "filesystem"
 app.config["PERMANENT_SESSION_LIFETIME"] = 6000
 app.config["SECRET_KEY"] = secrets.token_hex(16)
 Session(app)
